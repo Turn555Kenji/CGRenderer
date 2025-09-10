feb02057732ffaf07b72ec4838952e4ec7af9a4d
Papuiz <luizfuzeto@gmail.com>
2025-09-10 20:54:51 -0300

    Atualizei a linha para a nova versão

--- line.cpp
+++ line.cpp
@@ -1,13 +1,31 @@
 #include "line.h"
+#include <QPainter>
+#include "matrix.h" // <-- CORREÇÃO: ADICIONADO AQUI
 
-Line::Line(Point initP1, Point initP2, int id, const QString &name, Type type)
-    : Obj(id, name, type), p1(initP1), p2(initP2) {
+// Construtor: inicializa o objeto e copia os pontos recebidos
+Line::Line(const Point& p1, const Point& p2, int id, QString name)
+    : Obj(id, name, Type::LINE), p1(p1), p2(p2) {
+    // A inicialização é feita na lista acima, o corpo pode ser vazio
 }
 
-void Line::draw(QPainter *painter) {
+// Método para desenhar a linha na tela
+void Line::draw(QPainter painter) {
+    int x1 = static_cast<int>(p1[0][0]);
+    int y1 = static_cast<int>(p1[1][0]);
 
+    int x2 = static_cast<int>(p2[0][0]);
+    int y2 = static_cast<int>(p2[1][0]);
+
+    painter->drawLine(x1, y1, x2, y2);
 }
 
-Obj* Line::transform(Matrix m) {
+// Método para aplicar uma transformação matricial aos dois pontos da linha
+Obj Line::transform(const Matrix& m) {
+    /*Matrix result_p1 = m * p1;
+    Matrix result_p2 = m * p2;
 
+    this->p1 = Point(result_p1[0][0], result_p1[1][0]);
+    this->p2 = Point(result_p2[0][0], result_p2[1][0]);
+    */
+    return this;
 }
