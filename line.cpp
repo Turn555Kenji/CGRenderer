e689cf4169adf192b134c24da32a02066598b28e
Marcos Passos <marcospassos453@gmail.com>
2025-12-01 15:26:09 -0300

    adequacao do metodo de clipping e ajuste da perpec

--- line.cpp
+++ line.cpp
@@ -1,7 +1,12 @@
 #include "line.h"
 #include <QPainter>
 #include "matrix.h"
-
+#include <QDebug>
+const int INSIDE = 0;
+const int LEFT   = 1;
+const int RIGHT  = 2;
+const int BOTTOM = 4;
+const int TOP    = 8;
 // Construtor: inicializa o objeto e copia os pontos recebidos
 Line::Line(const Point& p1, const Point& p2, int id, QString name)
     : Obj(id, name, Type::Line), p1(p1), p2(p2) {
@@ -17,49 +22,150 @@ void Line::draw(QPainter *painter, double dist, bool perspectflag,
     Point P2_proj = p2;
 
     if(perspectflag){
+        if (dist == 0) dist = 1;
+
         double centerX = (Xwmin + Xwmax) / 2.0;
         double centerY = (Ywmin + Ywmax) / 2.0;
+
         // Matriz de projeção
         Matrix p(4, 4);
         p[0][0] = 1; p[0][1] = 0; p[0][2] = 0; p[0][3] = 0;
         p[1][0] = 0; p[1][1] = 1; p[1][2] = 0; p[1][3] = 0;
         p[2][0] = 0; p[2][1] = 0; p[2][2] = 1; p[2][3] = 0;
-        p[3][0] = 0; p[3][1] = 0; p[3][2] = 1/dist; p[3][3] = 1;
+        p[3][0] = 0; p[3][1] = 0; p[3][2] = 1.0/dist; p[3][3] = 1;
 
-        // Aplica em P1
-        Point auxP = P1_proj;
-        auxP[0][0] -= centerX;
-        auxP[1][0] -= centerY;
-        Matrix m = p * auxP;
-        if (m[3][0] != 0) {
-            P1_proj[0][0] = m[0][0] / m[3][0];
-            P1_proj[1][0] = m[1][0] / m[3][0];
-            P1_proj[2][0] = m[2][0] / m[3][0];
+
+        // --- PONTO 1 ---
+        Point auxP1 = P1_proj;
+        auxP1[0][0] -= centerX;
+        auxP1[1][0] -= centerY;
+
+        Matrix m1 = p * auxP1;
+        if (m1[3][0] != 0) {
+
+            P1_proj[0][0] = m1[0][0] / m1[3][0];
+            P1_proj[1][0] = m1[1][0] / m1[3][0];
+            P1_proj[2][0] = m1[2][0] / m1[3][0];
+
+            // voltar oara coordenada de munda
+            P1_proj[0][0] += centerX;
+            P1_proj[1][0] += centerY;
         }
 
-        // Aplica em P2
-        auxP = P2_proj;
-        auxP[0][0] -= centerX;
-        auxP[1][0] -= centerY;
-        m = p * auxP;
-        if (m[3][0] != 0) {
-            P2_proj[0][0] = m[0][0] / m[3][0];
-            P2_proj[1][0] = m[1][0] / m[3][0];
-            P2_proj[2][0] = m[2][0] / m[3][0];
+        // --- PONTO 2 ---
+        Point auxP2 = P2_proj;
+        auxP2[0][0] -= centerX;
+        auxP2[1][0] -= centerY;
+
+        Matrix m2 = p * auxP2;
+        if (m2[3][0] != 0) {
+            P2_proj[0][0] = m2[0][0] / m2[3][0];
+            P2_proj[1][0] = m2[1][0] / m2[3][0];
+            P2_proj[2][0] = m2[2][0] / m2[3][0];
+
+            P2_proj[0][0] += centerX;
+            P2_proj[1][0] += centerY;
         }
     }
+
+    double wx1 = P1_proj[0][0];
+    double wy1 = P1_proj[1][0];
+    double wx2 = P2_proj[0][0];
+    double wy2 = P2_proj[1][0];
+
+
+    bool visible = clipplingCohen(wx1, wy1, wx2, wy2, Xwmin, Ywmin, Xwmax, Ywmax);
+    if (!visible) {
+        return;
+    }
+
     // Ponto 1: Mundo -> NDC -> Viewport (Usando P1_proj)
-    Point P1_ndc = P1_proj.normalize(Xwmin, Ywmin, Xwmax, Ywmax);
-    double x1 = Xvpmin + (P1_ndc[0][0] + 1.0) / 2.0 * (Xvpmax - Xvpmin);
-    double y1 = Yvpmin + (1.0 - P1_ndc[1][0]) / 2.0 * (Yvpmax - Yvpmin);
+    double ndcX1 = -1.0 + 2.0 * (wx1 - Xwmin) / (Xwmax - Xwmin);
+    double ndcY1 = -1.0 + 2.0 * (wy1 - Ywmin) / (Ywmax - Ywmin);
+
+    double x1 = Xvpmin + (ndcX1 + 1.0) / 2.0 * (Xvpmax - Xvpmin);
+    double y1 = Yvpmin + (1.0 - ndcY1) / 2.0 * (Yvpmax - Yvpmin);
 
     // Ponto 2: Mundo -> NDC -> Viewport (Usando P2_proj)
-    Point P2_ndc = P2_proj.normalize(Xwmin, Ywmin, Xwmax, Ywmax);
-    double x2 = Xvpmin + (P2_ndc[0][0] + 1.0) / 2.0 * (Xvpmax - Xvpmin);
-    double y2 = Yvpmin + (1.0 - P2_ndc[1][0]) / 2.0 * (Yvpmax - Yvpmin);
+    double ndcX2 = -1.0 + 2.0 * (wx2 - Xwmin) / (Xwmax - Xwmin);
+    double ndcY2 = -1.0 + 2.0 * (wy2 - Ywmin) / (Ywmax - Ywmin);
+    double x2 = Xvpmin + (ndcX2 + 1.0) / 2.0 * (Xvpmax - Xvpmin);
+    double y2 = Yvpmin + (1.0 - ndcY2) / 2.0 * (Yvpmax - Yvpmin);
 
     painter->drawLine(static_cast<int>(x1), static_cast<int>(y1), static_cast<int>(x2), static_cast<int>(y2));
 }
+int Line::Regiao(double x, double y, double Xwmin, double Ywmin, double Xwmax, double Ywmax)
+{
+    int code = INSIDE;
+
+    if (x < Xwmin)
+        code |= LEFT;
+    else if (x > Xwmax)
+        code |= RIGHT;
+
+    if (y < Ywmin)
+        code |= BOTTOM;
+    else if (y > Ywmax)
+        code |= TOP;
+
+    return code;
+}
+
+
+bool Line::clipplingCohen(double& x1, double& y1, double& x2, double& y2, double Xwmin, double Ywmin, double Xwmax, double Ywmax)
+{
+    int code1 = Regiao(x1, y1, Xwmin, Ywmin,Xwmax,Ywmax);
+    int code2 = Regiao(x2, y2, Xwmin, Ywmin,Xwmax,Ywmax);
+    bool accept = false;
+
+    while (true) {
+        if ((code1 == 0) && (code2 == 0)) { //dentro da window
+            accept = true;
+            break;
+        } else if (code1 & code2) {
+            // Caso2 os dois estão fora
+            accept = false;
+            break;
+        } else {
+            // Caso 3 segmento de reta
+            double x, y;
+            int codeOut;
+            // seleciona a parte da reta que está pra fora
+            if (code1 != 0) {
+                codeOut = code1;
+            } else {
+                codeOut = code2;
+            }
+
+            // interseção.
+            if (codeOut & TOP) { //acima
+                x = x1 + (x2 - x1) * (Ywmax - y1) / (y2 - y1);
+                y = Ywmax;
+            } else if (codeOut & BOTTOM) {//abaixo
+                x = x1 + (x2 - x1) * (Ywmin - y1) / (y2 - y1);
+                y = Ywmin;
+            } else if (codeOut & RIGHT) {//direita
+                y = y1 + (y2 - y1) * (Xwmax - x1) / (x2 - x1);
+                x = Xwmax;
+            } else if (codeOut & LEFT) {   //esquerda
+                y = y1 + (y2 - y1) * (Xwmin - x1) / (x2 - x1);
+                x = Xwmin;
+            }
+
+            if (codeOut == code1) {
+                x1 = x;
+                y1 = y;
+                code1 = Regiao(x1, y1, Xwmin, Ywmin,Xwmax,Ywmax);
+            } else {
+                x2 = x;
+                y2 = y;
+                code2 = Regiao(x2, y2, Xwmin, Ywmin,Xwmax,Ywmax);
+            }
+        }
+    }
+    return accept;
+}
+
 
 // Método para aplicar uma transformação matricial aos dois pontos da linha
 Obj* Line::transform(Matrix m){
--- line.h
+++ line.h
@@ -7,6 +7,8 @@
 
 class Line : public Obj {
 public:
+
+
     // Construtor que recebe os dois pontos que definem a linha
     Line(const Point& p1, const Point& p2, int id, QString name);
 
@@ -15,6 +17,8 @@ public:
     void draw(QPainter *painter, double dist, bool perspectflag,
               double Xwmin, double Ywmin, double Xwmax, double Ywmax,
               double Xvpmin, double Yvpmin, double Xvpmax, double Yvpmax) override;
+    int Regiao(double x, double y, double Xwmin, double Ywmin, double Xwmax, double Ywmax);
+    bool clipplingCohen(double& x1, double& y1, double& x2, double& y2, double Xwmin, double Ywmin, double Xwmax, double Ywmax);
     Obj* transform(const Matrix m) override;
 
     Point getP1() const {return p1; }
